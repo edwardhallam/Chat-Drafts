a38c1cf547f5d717fb71d4b0f93298135403e3ba
Edward Hallam <edwardhallam@gmail.com>
2025-03-18 12:16:33 -0700

    Initial Commit

---
+++ Wall Of Text/Wall_Of_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wall_Of_TextApp.swift
+//  Wall Of Text
+//
+//  Created by Edward Hallam on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Wall_Of_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
